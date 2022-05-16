8e4f7260da8b770c84b93a88be83ff36ee21c398
ubs <ubs4939@naver.com>
2022-05-16 15:50:13 +0900

    조인

---
+++ src/main/java/jpql/main/JpaJoin.java
@@ -0,0 +1,58 @@
+package jpql.main;
+
+import jpql.Member;
+import jpql.Team;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaJoin {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+
+        try {
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("member");
+            member.setAge(10);
+
+
+            member.setTeam(team);
+
+
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+//            String query = "select m from Member m join m.team t";
+//            String query = "select m from Member m left join m.team t";
+            String query = "select m from Member m,Team t where m.username = t.name";
+            List<Member> result = em.createQuery(query, Member.class)
+                    .getResultList();
+
+            System.out.println("result.size() = " + result.size());
+            for (Member member1 : result) {
+                System.out.println("member1 = " + member1);
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
