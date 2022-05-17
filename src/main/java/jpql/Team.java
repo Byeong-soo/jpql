5aeef2066fe714a7e764f49b95959651873b2907
ubs <ubs4939@naver.com>
2022-05-17 18:09:53 +0900

    페치조인

--- src/main/java/jpql/Team.java
+++ src/main/java/jpql/Team.java
@@ -1,6 +1,8 @@
 package jpql;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.BatchSize;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
---
+++ src/main/java/jpql/main/JpaFetchJoin.java
@@ -0,0 +1,78 @@
+package jpql.main;
+
+import jpql.Member;
+import jpql.Team;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class JpaFetchJoin {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+
+        try {
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
+
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
+
+            Member member1 = new Member();
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
+            em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
+            em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
+
+
+            em.flush();
+            em.clear();
+
+
+            // manyToOne
+//            String query = "select m From Member m join fetch m.team";
+//            List<Member> result = em.createQuery(query, Member.class)
+//                    .getResultList();
+//
+//            for (Member member : result) {
+//                System.out.println("member = " + member.getUsername() + ", " + member.getTeam().getName());
+//                // 회원1, 팀A(SQL)
+//                // 회원2, 팀A(1차캐쉬)
+//                // 회원3, 팀B(SQL)
+//
+//                // 회원 100명 -> N + 1
+//            }
+
+            // oneToMany 일대다 join 에서는 데이터 뻥튀기 될 수 있음.
+            String query = "select distinct t from Team t join fetch t.members";
+            List<Team> resultList = em.createQuery(query, Team.class)
+                    .getResultList();
+
+            for (Team team : resultList) {
+                System.out.println("team = " + team.getName() + ", "+ team.getMembers().size());
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+            e.printStackTrace();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
