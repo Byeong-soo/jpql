192d87eba927d172f0f5fc700e34b7a3482a9fca
ubs <ubs4939@naver.com>
2022-05-17 18:30:09 +0900

    namedQuery

--- src/main/java/jpql/Member.java
+++ src/main/java/jpql/Member.java
@@ -7,12 +7,19 @@ import javax.persistence.*;
 
 @Entity
 @Getter @Setter
+@NamedQuery(
+        name = "Member.findByUsername",
+        query = "select m from Member m where m.username = :username"
+)
 public class Member {
 
     @Id @GeneratedValue
     private Long id;
     private String username;
     private int age;
+
+    @Enumerated(EnumType.STRING)
+    private MemberType type;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "TEAM_ID")
